590ab30d996db825b3545856e046d22b549cc69c
Ervin Chow <ervinchow@gmail.com>
2015-06-24 10:46:36 -0700

    adds logic that changes navbar styling based on viewport size

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 $(window).scroll(function() {
-if ($(this).scrollTop() > 0.5){  
-    $('.navbar').addClass("sticky");
-  }
-  else{
-    $('.navbar').removeClass("sticky");
-  }
+    if ($(this).scrollTop() > 0.5) {
+        $('.navbar').addClass("sticky");
+    } else {
+        $('.navbar').removeClass("sticky");
+    }
+
+    if ($(window).width() < 769) {
+        $('.navbar-default').removeClass("sticky").removeClass("navbar-fixed-top").addClass("navbar-static-top");
+    }
+
+    if ($(window).width() > 768) {
+        $('.navbar-default').addClass('navbar-fixed-top');
+    }
 });
